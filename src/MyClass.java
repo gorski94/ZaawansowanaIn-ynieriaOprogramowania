fc775a834828d475aa4303513d8a6fac66fc75a9
gorski94 <r.goral10@wp.pl>
2019-10-06 10:12:19 +0200

    Update MyClass.java

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,5 +2,6 @@ public class MyClass {
     public static void main(String[] args){
         System.out.print("Hello World");
         System.out.println("Drugi commit");
+        System.out.println("Trzeci commit");
     }
 }
